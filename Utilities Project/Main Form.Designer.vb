df4ffccf2f2ed5a543cc3416b8c8fac52a4b640c
Ian Skelskey <ianskelskey@gmail.com>
2020-04-09 21:57:00 -0700

    completed part a of exercise 7 on page 524

--- Utilities Project/Main Form.Designer.vb
+++ Utilities Project/Main Form.Designer.vb
@@ -32,23 +32,33 @@ Partial Class frmMain
         Me.BillsTableAdapter = New Utilities_Project.UtilitiesDataSetTableAdapters.BillsTableAdapter()
         Me.TableAdapterManager = New Utilities_Project.UtilitiesDataSetTableAdapters.TableAdapterManager()
         Me.BillsBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
         Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
-        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
         Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
         Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
-        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
-        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
         Me.BillsBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
         Me.BillsDataGridView = New System.Windows.Forms.DataGridView()
         Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblElectricity = New System.Windows.Forms.Label()
+        Me.lblWater = New System.Windows.Forms.Label()
+        Me.lblGas = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.UtilitiesDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.BillsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.BillsBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -82,6 +92,7 @@ Partial Class frmMain
         Me.BillsBindingNavigator.BindingSource = Me.BillsBindingSource
         Me.BillsBindingNavigator.CountItem = Me.BindingNavigatorCountItem
         Me.BillsBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.BillsBindingNavigator.ImageScalingSize = New System.Drawing.Size(32, 32)
         Me.BillsBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.BillsBindingNavigatorSaveItem})
         Me.BillsBindingNavigator.Location = New System.Drawing.Point(0, 0)
         Me.BillsBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
@@ -90,9 +101,34 @@ Partial Class frmMain
         Me.BillsBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
         Me.BillsBindingNavigator.Name = "BillsBindingNavigator"
         Me.BillsBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
-        Me.BillsBindingNavigator.Size = New System.Drawing.Size(405, 25)
+        Me.BillsBindingNavigator.Size = New System.Drawing.Size(393, 39)
         Me.BillsBindingNavigator.TabIndex = 0
         Me.BillsBindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(36, 36)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 36)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(36, 36)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
         '
         'BindingNavigatorMoveFirstItem
         '
@@ -100,7 +136,7 @@ Partial Class frmMain
         Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
         Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
         Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(36, 36)
         Me.BindingNavigatorMoveFirstItem.Text = "Move first"
         '
         'BindingNavigatorMovePreviousItem
@@ -109,13 +145,13 @@ Partial Class frmMain
         Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
         Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
         Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(36, 36)
         Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
         '
         'BindingNavigatorSeparator
         '
         Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 39)
         '
         'BindingNavigatorPositionItem
         '
@@ -126,17 +162,10 @@ Partial Class frmMain
         Me.BindingNavigatorPositionItem.Text = "0"
         Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
         '
-        'BindingNavigatorCountItem
-        '
-        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
-        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(35, 22)
-        Me.BindingNavigatorCountItem.Text = "of {0}"
-        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
-        '
         'BindingNavigatorSeparator1
         '
-        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 25)
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 39)
         '
         'BindingNavigatorMoveNextItem
         '
@@ -144,7 +173,7 @@ Partial Class frmMain
         Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
         Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
         Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(36, 36)
         Me.BindingNavigatorMoveNextItem.Text = "Move next"
         '
         'BindingNavigatorMoveLastItem
@@ -153,38 +182,20 @@ Partial Class frmMain
         Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
         Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
         Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(36, 36)
         Me.BindingNavigatorMoveLastItem.Text = "Move last"
         '
         'BindingNavigatorSeparator2
         '
-        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 25)
-        '
-        'BindingNavigatorAddNewItem
-        '
-        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
-        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorAddNewItem.Text = "Add new"
-        '
-        'BindingNavigatorDeleteItem
-        '
-        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
-        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 39)
         '
         'BillsBindingNavigatorSaveItem
         '
         Me.BillsBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
         Me.BillsBindingNavigatorSaveItem.Image = CType(resources.GetObject("BillsBindingNavigatorSaveItem.Image"), System.Drawing.Image)
         Me.BillsBindingNavigatorSaveItem.Name = "BillsBindingNavigatorSaveItem"
-        Me.BillsBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 22)
+        Me.BillsBindingNavigatorSaveItem.Size = New System.Drawing.Size(36, 36)
         Me.BillsBindingNavigatorSaveItem.Text = "Save Data"
         '
         'BillsDataGridView
@@ -194,10 +205,10 @@ Partial Class frmMain
         Me.BillsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.BillsDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4})
         Me.BillsDataGridView.DataSource = Me.BillsBindingSource
-        Me.BillsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.BillsDataGridView.Location = New System.Drawing.Point(0, 25)
+        Me.BillsDataGridView.Location = New System.Drawing.Point(12, 45)
         Me.BillsDataGridView.Name = "BillsDataGridView"
-        Me.BillsDataGridView.Size = New System.Drawing.Size(405, 366)
+        Me.BillsDataGridView.RowHeadersWidth = 82
+        Me.BillsDataGridView.Size = New System.Drawing.Size(369, 366)
         Me.BillsDataGridView.TabIndex = 1
         '
         'DataGridViewTextBoxColumn1
@@ -205,6 +216,7 @@ Partial Class frmMain
         Me.DataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
         Me.DataGridViewTextBoxColumn1.DataPropertyName = "Month"
         Me.DataGridViewTextBoxColumn1.HeaderText = "Month"
+        Me.DataGridViewTextBoxColumn1.MinimumWidth = 10
         Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
         Me.DataGridViewTextBoxColumn1.ReadOnly = True
         Me.DataGridViewTextBoxColumn1.Width = 68
@@ -217,6 +229,7 @@ Partial Class frmMain
         DataGridViewCellStyle1.NullValue = Nothing
         Me.DataGridViewTextBoxColumn2.DefaultCellStyle = DataGridViewCellStyle1
         Me.DataGridViewTextBoxColumn2.HeaderText = "Electricity"
+        Me.DataGridViewTextBoxColumn2.MinimumWidth = 10
         Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
         '
         'DataGridViewTextBoxColumn3
@@ -227,6 +240,7 @@ Partial Class frmMain
         DataGridViewCellStyle2.NullValue = Nothing
         Me.DataGridViewTextBoxColumn3.DefaultCellStyle = DataGridViewCellStyle2
         Me.DataGridViewTextBoxColumn3.HeaderText = "Water"
+        Me.DataGridViewTextBoxColumn3.MinimumWidth = 10
         Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
         '
         'DataGridViewTextBoxColumn4
@@ -237,13 +251,110 @@ Partial Class frmMain
         DataGridViewCellStyle3.NullValue = Nothing
         Me.DataGridViewTextBoxColumn4.DefaultCellStyle = DataGridViewCellStyle3
         Me.DataGridViewTextBoxColumn4.HeaderText = "Gas"
+        Me.DataGridViewTextBoxColumn4.MinimumWidth = 10
         Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(210, 485)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalc.TabIndex = 2
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(291, 485)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblElectricity
+        '
+        Me.lblElectricity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblElectricity.Location = New System.Drawing.Point(12, 448)
+        Me.lblElectricity.Name = "lblElectricity"
+        Me.lblElectricity.Size = New System.Drawing.Size(75, 20)
+        Me.lblElectricity.TabIndex = 4
+        '
+        'lblWater
+        '
+        Me.lblWater.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblWater.Location = New System.Drawing.Point(110, 448)
+        Me.lblWater.Name = "lblWater"
+        Me.lblWater.Size = New System.Drawing.Size(75, 20)
+        Me.lblWater.TabIndex = 5
+        '
+        'lblGas
+        '
+        Me.lblGas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGas.Location = New System.Drawing.Point(208, 448)
+        Me.lblGas.Name = "lblGas"
+        Me.lblGas.Size = New System.Drawing.Size(75, 20)
+        Me.lblGas.TabIndex = 6
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(306, 448)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(75, 20)
+        Me.lblTotal.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 432)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 15)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Electricity:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(107, 432)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 15)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Water:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(207, 432)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 15)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Gas:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(303, 432)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 15)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Total:"
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(405, 391)
+        Me.ClientSize = New System.Drawing.Size(393, 520)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblGas)
+        Me.Controls.Add(Me.lblWater)
+        Me.Controls.Add(Me.lblElectricity)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
         Me.Controls.Add(Me.BillsDataGridView)
         Me.Controls.Add(Me.BillsBindingNavigator)
         Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
@@ -284,4 +395,14 @@ Partial Class frmMain
     Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
     Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
     Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblElectricity As Label
+    Friend WithEvents lblWater As Label
+    Friend WithEvents lblGas As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- Utilities Project/Main Form.vb
+++ Utilities Project/Main Form.vb
@@ -23,6 +23,26 @@ Public Class frmMain
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'UtilitiesDataSet.Bills' table. You can move, or remove it, as needed.
         Me.BillsTableAdapter.Fill(Me.UtilitiesDataSet.Bills)
+    End Sub
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        me.Close()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim decElectricityTotal As Decimal
+        Dim decWaterTotal As Decimal
+        Dim decGasTotal As Decimal
+        Dim decBillsTotal As Decimal
+        For Each row As UtilitiesDataSet.BillsRow In UtilitiesDataSet.Bills
+            decElectricityTotal += row.Electricity
+            decWaterTotal += row.Water
+            decGasTotal += row.Gas
+        Next
+        decBillsTotal = decElectricityTotal + decWaterTotal + decGasTotal
+        lblElectricity.Text = decElectricityTotal.ToString("C2")
+        lblWater.Text = decWaterTotal.ToString("C2")
+        lblGas.Text = decGasTotal.ToString("C2")
+        lblTotal.Text = decBillsTotal.ToString("C2")
     End Sub
 End Class
